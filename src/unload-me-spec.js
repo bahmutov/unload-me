e50e4b0e0c3fbeb5113c6ae4a94a2e0fb115547b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-09-03 10:42:41 -0400

    initial setup

---
+++ src/unload-me-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* eslint-env mocha */
+const unloadMe = require('.')
+
+describe('unload-me', () => {
+  it('write this test', () => {
+    console.assert(unloadMe, 'should export something')
+  })
+})
